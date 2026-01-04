a8f5c7a7635f46e1e0e8520d476916fd5f6555d2
HangingWire1 <lishaw2425@gmail.com>
2026-01-04 11:48:56 +0630

    posts by category

--- blogs/admin.py
+++ blogs/admin.py
@@ -3,10 +3,12 @@ from blogs.models import Category, Blog
 
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    list_display = ("title","category","author","status","is_featured")
+    list_display = ("title","category_id","category","author","status","is_featured")
     search_fields = ("id","title","category__category_name","status")
     list_editable = ("is_featured",)
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("category_name","pk",)
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Blog, BlogAdmin)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from blogs.models import Category, Blog
+from django.shortcuts import render, get_object_or_404, redirect
+
 
 # Create your views here.
+def posts_by_category(request, category_id):
+    posts = Blog.objects.filter(status = 'Published', category = category_id)
+
+    #use try/except to do some actions if the category doesn't exist
+    try:
+        category = Category.objects.get(pk = category_id)
+    except:
+        #redirect the user to homepage
+        return redirect('home')
+
+    #shoe 404 page if the category doesn't exist
+    # category = get_object_or_404(Category, pk=category_id)
+
+    context = {
+        'posts': posts,
+        'category': category,
+    }
+    return render(request, 'posts_by_category.html', context)
